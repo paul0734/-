80191a8034b51fd9d54dcf3e5b1df2c96e4fc971
paul0734 <56014949+paul0734@users.noreply.github.com>
2021-10-02 11:19:43 +0900

    성적 출력
    성적 출력

---
+++ 2단계/9498.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] agrs){
+        Scanner sc = new Scanner(System.in);
+        int grade = sc.nextInt();
+        
+        if (grade>=90)
+            System.out.println("A");
+        else if(grade>=80)
+            System.out.println("B");
+        else if(grade>=70)
+            System.out.println("C");
+        else if(grade>=60)
+            System.out.println("D");
+        else
+            System.out.println("F");
+    }
+}
