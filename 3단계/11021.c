b89b6b4484407eebe097f7ad525cf4f8dda8239f
paul0734 <56014949+paul0734@users.noreply.github.com>
2021-10-03 14:42:25 +0900

    A+B __7
    A+B __7

---
+++ 3단계/11021.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int n,i,j;
+    scanf("%d", &n);
+    
+    for(int k=1;k<=n;k++){
+        scanf("%d %d", &i, &j);
+        printf("Case #%d: %d\n", k, i+j);
+    }
+}
